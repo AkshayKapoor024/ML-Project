23489281c58f9cb8ae40d953875f9e042ff0bbe3
AkshayKapoor024 <work.akshaykapoor24@gmail.com>
2026-03-16 13:24:32 +0530

    Added Deployement Files

--- application.py
+++ application.py
@@ -37,4 +37,4 @@ def predict():
         return render_template('home.html',results=predictions[0])
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000,host='0.0.0.0')
+    app.run(host='0.0.0.0')
